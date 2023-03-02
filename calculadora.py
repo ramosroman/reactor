44f20db146063788a02621ca00184685b5a1b895
Roman Ramos <ramosroman@gmail.com>
2023-03-01 20:51:11 -0400

    Se agrego la funcion resta y el archivo demo

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,8 @@ def suma(numeros1,numeros2):
     total = numeros1 + numeros2
     return total
 
+def suma(numeros1,numeros2):
+    total = numeros1 - numeros2
+    return total
 resultado = suma(num1,num2)
 print('El resultado es: ',resultado)
